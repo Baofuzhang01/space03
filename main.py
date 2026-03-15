e7f0b0834099d46838b530244a4ed684da67c381
Qingwen Zhang <129290660+BAOfuZhan@users.noreply.github.com>
2026-03-15 17:29:13 +0800

    1

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def _format_seat_number(seat_num: int) -> str:
 
 
 SLEEPTIME = 0.1  # 每次抢座的间隔（减少到0.05秒以加快速度）
-ENDTIME = "20:00:40"  # 根据学校的预约座位时间+1min即可
+ENDTIME = "08:00:40"  # 根据学校的预约座位时间+1min即可
 
 ENABLE_SLIDER = False  # 是否有滑块验证（调试阶段先关闭）
 ENABLE_TEXTCLICK = False  # 是否有选字验证码（需要图灵云打码平台）
